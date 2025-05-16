1a7d41acc4efb4a1a65b8a522a018ca019f45dda
Mchael254 <michealotieno729@gmail.com>
2025-05-16 17:44:26 +0300

    profile pic update

--- src/app/profile/membership/membership.component.ts
+++ src/app/profile/membership/membership.component.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { faPen } from '@fortawesome/free-solid-svg-icons';
 import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subject, takeUntil } from 'rxjs';
 
 import { ResponsesService } from 'src/app/services/responses.service';
 import { updateProfileImage } from 'src/app/store/auth/auth.actions';
@@ -16,12 +16,15 @@ import { AuthState } from 'src/app/store/auth/auth.reducer';
 export class MembershipComponent {
   @ViewChild('fileInput') fileInputRef!: ElementRef<HTMLInputElement>;
   profile$!: Observable<AuthState>;
+  private destroy$ = new Subject<void>();
 
   constructor(private http: HttpClient,
     private response: ResponsesService,
     private store: Store<{ auth: AuthState }>) {
     this.profile$ = this.store.pipe(select('auth'));
+
   }
+
 
   previewImage: string | null = null;
   faPen = faPen;
@@ -29,6 +32,16 @@ export class MembershipComponent {
   fileInput: any;
   selectedFile: File | null = null;
   isUploading = false;
+  profileId: string | null = null;
+
+  ngOnInit(): void {
+    this.store
+      .pipe(select('auth'), takeUntil(this.destroy$))
+      .subscribe((state) => {
+        this.profileId = state.user?.id || null;
+      });
+  }
+
 
   onImageSelected(event: Event): void {
     const file = (event.target as HTMLInputElement).files?.[0];
@@ -52,28 +65,27 @@ export class MembershipComponent {
   }
 
   uploadImage(): void {
-    if (!this.selectedFile) return;
+    if (!this.selectedFile || !this.profileId) {
+      this.response.showError('No picture selected');
+      return;
+    }
 
     this.response.showSpinner();
 
-    const userId = '99443178-66b5-44bb-9b0a-f119e6b4083e';
-
     const formData = new FormData();
     formData.append('image', this.selectedFile);
-    formData.append('userId', userId);
+    formData.append('userId', this.profileId);
     this.http
       .post<any>('http://localhost:3000/user/upload-profile-picture', formData)
+      .pipe(takeUntil(this.destroy$))
       .subscribe({
         next: (response) => {
           this.previewImage = null;
           this.selectedFile = null;
           this.fileInputRef.nativeElement.value = '';
           this.response.showSuccess('Profile picture updated');
-
           this.store.dispatch(updateProfileImage({ profile_image: response.imageUrl }));
-
           this.response.hideSpinner();
-
         },
         error: (err) => {
           console.error('Upload failed', err);
