41cf63cd9ca4025af1e894433d0815f8f00bd42e
Mchael254 <michealotieno729@gmail.com>
2025-09-15 13:17:41 -0600

    Stop tracking environment files

--- src/environments/environment.development.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    apiUrl: 'https://a-riders-backend.onrender.com',
-    localUrl: 'http://localhost:5300',
-    paymentUrl:'https://mpesa-dogr.onrender.com',
-    frontendUrl: 'http://localhost:4200',
-    production: false,
-    supabaseUrl: 'https://aidnxywieovjglfrcwty.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpZG54eXdpZW92amdsZnJjd3R5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMjI2MTE0MSwiZXhwIjoyMDQ3ODM3MTQxfQ.pwVHd0zEeJaYRlCoJ9JvO25RE2A15BgEUTzgDSX8M5E',
-};
